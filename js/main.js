fc9d29291ecdc92881c8056fc9ec7577dba973b7
Vlad Vobiachenko <109553178+vladosi1@users.noreply.github.com>
2022-12-11 23:44:11 +0200

    sec commit
    +burger btn

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-
 const navBtn = document.querySelector('.nav__button');
 const mobileNav = document.querySelector('.mobile__nav');
 
 navBtn.addEventListener('click', function() {
+	
 	mobileNav.classList.toggle('mobile__nav__active');
+	navBtn.classList.toggle('nav__button__close');
+	
 });
 
